9a97e92be0c807b10c385c6491cf822767f01bfd
gan74 <gregoire.angerand@epfl.ch>
2017-10-15 15:37:01 +0200

    Add basic binning for events

--- projects/project1/scripts/run.py
+++ projects/project1/scripts/run.py
@@ -1,5 +1,23 @@
+from collections import defaultdict
 import proj1_helpers as helpers
+import numpy as np
 
 
 
 yb, input_data, ids = helpers.load_csv_data("../data/train.csv", True)
+
+
+
+def event_mask(ev):
+	"""integer value representing the category of the event"""
+	return sum((v != -999) << j for j, v in enumerate(ev))
+
+def bucket_events(data):
+	"""buckets events by category: returns a list containing list of indexes for each category"""
+	d = defaultdict(list)
+	for i, ev in enumerate(data):
+		d[event_mask(ev)].append(i)
+	return d.values()
+
+
+print(len(bucket_events(input_data)))
