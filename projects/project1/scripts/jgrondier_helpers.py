299a68779254153d76a1bd1a3684fb9c24e3f916
jgrondier <juliengrondier@gmail.com>
2017-10-22 17:53:54 +0200

    Changed > to >=

--- projects/project1/scripts/jgrondier_helpers.py
+++ projects/project1/scripts/jgrondier_helpers.py
@@ -10,11 +10,12 @@ def pearson_coeffs(y, tX, threshold=0):
     for i, col in enumerate(tX.T) :
         l.append([i, np.corrcoef([y, col])[0][1]])
 
-    return [x for x in l if abs(x[1]) > threshold]
+    return [x for x in l if abs(x[1]) >= threshold]
 #    for e in sorted_l:
 #        print("{} : {}".format(e[0], e[1]))
 
 #Returns a stripped version of the data where we ignore data with pearson coefficients under the treshold
+#First element is the indexes of columns kept, the second element is the stripped data
 def build_pearson_coeffs_data(y, tX, threshold=0):
 
     coeffs = np.array(pearson_coeffs(y, tX, threshold)).T.astype(np.int64);
