1591ed08623d41bee20ad9836901a62185e72fee
gan74 <gregoire.angerand@epfl.ch>
2017-10-22 15:23:14 +0200

    better code

--- projects/project1/scripts/run.py
+++ projects/project1/scripts/run.py
@@ -6,57 +6,63 @@ import numpy as np
 
 def bucket_events(data):
 	"""bucket events by PRI_jet_num"""
-	return [(np.where(data[:, 22] == i)) for i in range(0, 4)]	
+	return [(np.where(data[:, 24] == i)) for i in range(0, 4)]	
+
+
+def remove_undef(data):
+	mean = data[data != -999].mean()
+	data[np.where(data == -999)] = mean
+	return data
 
 def prepare_data(data):
 	"""transforms mass and filters columns"""
-	data[:, 0] = np.where(data[:, 0] < 0, -1, 1)
-	x = standardize(data[:, data[0, :] != -999])
-	return np.c_[np.ones(len(x)), x]
-
+	cols = np.array([remove_undef(c) for c in data.T])
+	return np.c_[np.ones(len(data)), 
+				 np.where(data[:, 0] < 0, -1, 1), # mass bool
+				 cols.T]
 
 
 
 def linreg(y, tx):
 	"""performs basic linear regression"""
 	assert(len(y) == len(tx))
+	assert(len(y) > 0)
 
 	success = 0
 
-	tx = prepare_data(tx)
-	losses, ws = gradient_descent(y, tx, np.zeros(tx.shape[1]), 5000, 0.01)
-	min_loss = min(losses)
-	best_w = next(w for l, w in zip(losses, ws) if l == min_loss)
+	tx = standardize(tx)
 
-	for r, ev in zip(y, tx):
-		prediction = -1 if best_w.dot(ev) < 0 else 1
+	losses, ws = gradient_descent(y, tx, np.zeros(tx.shape[1]), 5000, 0.005)
+	min_loss = min(losses)
+
+	return next(w for l, w in zip(losses, ws) if l == min_loss)
+
+
+
+def bench(w, y, tx):
+	success = 0
+	for r, ev in zip(y, standardize(tx)):
+		#predictions = [-1 if we.dot(ev) < 0 else 1 for we in w]
+		#prediction = -1 if np.sum(predictions) < 0 else 1
+		prediction = -1 if w.dot(ev) < 0 else 1
 		success += 1 if prediction == int(r) else 0
 
 	print("  success rate =", (success / float(len(y)) * 100), "\n")
+	return success
 
-	return best_w, success
 
 
 if __name__ == "__main__":
     
     yb, input_data, ids = helpers.load_csv_data("../data/small.csv", True)
+    input_data = prepare_data(input_data)
 
-    no_mass = input_data[:, 0] == -999
-    no_mass_y = yb[no_mass]
-    no_mass_data = input_data[no_mass]
+    success = 0
+    for i, b in enumerate(bucket_events(input_data)):
+        print("Group", ["a:", "b:", "c:", "d:"][i], "(" + str(len(b[0])), "items)")
+        group_w = linreg(yb[b], input_data[b])
+        success += bench(group_w, yb[b], input_data[b])
 
-    print("Without mass:")
-    _, success = linreg(no_mass_y, no_mass_data)
-
-    with_mass = input_data[:, 0] != -999
-    mass_y = yb[with_mass]
-    mass_data = input_data[with_mass]
-
-    for i, b in enumerate(bucket_events(mass_data)):
-        print("Group", ["a:", "b:", "c:", "d:"][i])
-        _, s = linreg(mass_y[b], mass_data[b])
-        success += s
-
-    print("\ntotal =", (success / len(yb) * 100))
+    print("\ntotal average =", (success / len(yb) * 100))
 
 
