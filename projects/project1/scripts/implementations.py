312a49dec301e1e71f24cf9aac380f225dd9e720
gan74 <gregoire.angerand@epfl.ch>
2017-10-30 18:22:37 +0100

    Code cleanup & comments

--- projects/project1/scripts/implementations.py
+++ projects/project1/scripts/implementations.py
@@ -1,8 +1,6 @@
 
 import numpy as np
 
-from time import time
-
 try: from tqdm import tqdm
 except: tqdm = lambda x: x
 
@@ -172,8 +170,6 @@ def penalized_logistic_regression(y, tx, w, lambda_, loss_function = calculate_l
     g += lambda_ * w
 
     hessian=0 #Set to 0 because not used and creates memory errors
-    #hessian = calculate_hessian(y, tx, w)
-    #hessian +=  0.5 * lambda_ * np.linalg.norm(w) ** 2
 
 
     return loss, g , hessian
@@ -183,24 +179,15 @@ def learning_by_penalized_gradient(y, tx, w, gamma, lambda_, loss_function = cal
     Do one step of gradient descent, using the penalized logistic regression.
     Return the loss and updated w.
     """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # return loss, gradient: TODO
-    # ***************************************************
 
     loss, g, hessian = penalized_logistic_regression(y, tx, w, lambda_, loss_function)
-
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # update w: TODO
-    # ***************************************************
 
     w = w - gamma*g
 
 
     return loss, w
 
-def cross_validation_datasets(y, tx, k_fold, seed = time()):
+def cross_validation_datasets(y, tx, k_fold, seed = 42):
     k_indices = build_k_indices(y, k_fold, seed)
     """return the loss of ridge regression."""
     for k in range(k_fold):
@@ -236,3 +223,182 @@ def reg_logistic_regression(y, tx, lambda_=0.01, initial_w=None, max_iters=50, g
             return loss, w
 
     return losses[-1], w
+
+
+def uniq_count(t):
+    """returns the number of unique values in t """
+    vals = set()
+    for x in t:
+        vals.add(x)
+    return len(vals)
+
+def standardize_matrix(data):
+    """standardize matrix column by column"""
+    return np.array([standardize(c) for c in data.T]).T
+
+
+def bucket_events(data):
+    """bucket events by PRI_jet_num"""
+    return [np.where(data[:, 22] == i)[0] for i in range(0, 4)]
+
+def remove_undef(data):
+    """replace -999 by the mean"""
+    data = np.copy(data)
+    mean = data[data != -999].mean()
+    data[np.where(data == -999)] = mean
+    return data
+
+def columns(data):
+    """Returns an matrix without undefined values and specified colums"""
+    return np.array([remove_undef(c) for i, c in enumerate(data.T) if i not in [22]])#[14, 15, 17, 18, 22, 24, 25, 27, 28]])
+
+def analyse_data(data):
+    """Returns ivertibles columns and sqrt columns"""
+    cols = columns(data)
+    pos_cols = [i for i, c in enumerate(cols) if c.min() > 0]
+    nez_cols = [i for i, c in enumerate(cols) if 0 not in c]
+    return (pos_cols, nez_cols)
+
+def prepare_data(data, analysed, degree):
+    """transforms mass and add derivated features"""
+    cols = columns(data)
+    polys = [np.array([np.power(c, deg) for c in cols]).T for deg in range(2, degree + 1)]
+    #boolified = np.array([np.where(c < 0, -1, 1) for c in cols])
+
+    matrix = np.column_stack([cols.T, np.log(cols[analysed[0]]).T, np.reciprocal(cols[analysed[1]]).T] + polys)
+    polys = cols = pos_cols = nez_cols = None
+    return np.c_[np.ones(len(data)),
+                 np.where(data[:, 0] < 0, -1, 1), # mass bool
+                 standardize_matrix(matrix)]
+
+
+
+def linreg(y, tx):
+    """performs basic linear regression"""
+    assert(len(y) == len(tx))
+    assert(len(y) > 0)
+
+    losses, ws = gradient_descent(y, tx, np.zeros(tx.shape[1]), 5000, 0.01)
+    min_loss = min(losses)
+    return next(w for l, w in zip(losses, ws) if l == min_loss)
+
+
+def compute_ridge_rmse(yb, raw_data, lambda_, degree):
+
+    """Returns the RMSE for one specific lambda_, degree combination"""
+
+    analysed = analyse_data(raw_data)
+
+    rmse = [[], [], [], []]
+    for (test_y, raw_test_x, train_y, raw_train_x) in cross_validation_datasets(yb, raw_data, 4):
+        train_x = prepare_data(raw_train_x, analysed, degree)
+
+        pri_train_buckets = bucket_events(raw_train_x)
+        pri_w = [ridge_regression(train_y[b], train_x[b], lambda_) for b in pri_train_buckets]
+
+        pri_train_buckets = train_x = train_y = raw_train_x = None
+
+        test_x = prepare_data(raw_test_x, analysed, degree)
+        pri_test_buckets = bucket_events(raw_test_x)
+
+        for i, b in enumerate(pri_test_buckets):
+            rmse_te = np.sqrt(compute_loss_MSE(test_y[b], test_x[b], pri_w[i]))
+            rmse[i].append(rmse_te)
+
+    return [np.sum(r) for r in rmse]
+
+def compute_ridge_fail_rate(yb, raw_data, lambda_, degree):
+
+    """Returns the fail rate for one specific (degree, lambda_) combination"""
+
+    analysed = analyse_data(raw_data)
+
+    errors = [0, 0, 0, 0]
+    totals = [0, 0, 0, 0]
+    for (test_y, raw_test_x, train_y, raw_train_x) in cross_validation_datasets(yb, raw_data, 4, 42):
+        train_x = prepare_data(raw_train_x, analysed, degree)
+
+        pri_train_buckets = bucket_events(raw_train_x)
+        pri_w = [ridge_regression(train_y[b], train_x[b], lambda_) for b in pri_train_buckets]
+
+        pri_train_buckets = train_x = train_y = raw_train_x = None
+
+        test_x = prepare_data(raw_test_x, analysed, degree)
+        pri_test_buckets = bucket_events(raw_test_x)
+
+
+        for i, ev in enumerate(test_x):
+            pri = int(raw_test_x[i, 22])
+            assert(pri >= 0 and pri < 4)
+            x = pri_w[pri].dot(ev)
+            pred = -1 if x < 0 else 1
+            errors[pri] += 0 if pred == test_y[i] else 1
+            totals[pri] += 1
+
+    return [e / t for e, t in zip(errors, totals)]
+
+def compute_logit_fail_rate(yb, raw_data, lambda_, degree, gamma, threshold):
+
+    """Returns the fail rate for one specific (degree, lambda_, gamma) combination with a logistic regression"""
+
+    analysed = analyse_data(raw_data)
+
+    errors = [0, 0, 0, 0]
+    totals = [0, 0, 0, 0]
+    for (test_y, raw_test_x, train_y, raw_train_x) in cross_validation_datasets(yb, raw_data, 4, 42):
+        train_x = prepare_data(raw_train_x, analysed, degree)
+
+        pri_train_buckets = bucket_events(raw_train_x)
+        pri_w = []
+        for b in tqdm(pri_train_buckets):
+            w, loss = train_logistic(train_y[b], train_x[b], gamma, lambda_, 1000, threshold)
+            pri_w.append(w)
+
+        pri_train_buckets = train_x = train_y = raw_train_x = None
+
+        test_x = prepare_data(raw_test_x, analysed, degree)
+        pri_test_buckets = bucket_events(raw_test_x)
+
+
+        for i, ev in enumerate(test_x):
+            pri = int(raw_test_x[i, 22])
+            assert(pri >= 0 and pri < 4)
+            x = pri_w[pri].dot(ev)
+            pred = 0 if x < 0.5 else 1
+            errors[pri] += 0 if pred == test_y[i] else 1
+            totals[pri] += 1
+
+    return [e / t for e, t in zip(errors, totals)]
+
+def train_ridge_rmse(yb, raw_data, lambda_, degree):
+
+    """Prepare the data then trains using ridge regression on the given dataset"""
+
+    analysed = analyse_data(raw_data)
+
+    train_x = prepare_data(raw_data, analysed, degree)
+
+    pri_train_buckets = bucket_events(raw_data)
+    pri_w = [ridge_regression(yb[b], train_x[b], lambda_) for b in pri_train_buckets]
+
+    return pri_w
+
+
+def grid_search(y, raw_x, filename = "grid_results.csv", bucket_error_function = compute_ridge_fail_rate):
+
+    """Calcuates the fail rate for differente values of Lambda and Gamma and prints the fail rate for each bucket and lambda with a regular regression"""
+
+    with open(filename, 'w') as file:
+        file.write("lambda,degree,A error,B error,C error,D error\n")
+        degrees = range(1, 8)
+        lambdas = np.logspace(-5, 0, 100)
+        testing_errors = {}
+        for lambda_ in tqdm(lambdas):
+            for degree in tqdm(degrees):
+                errors = np.array(bucket_error_function(y, raw_x, lambda_, degree))
+                l = [lambda_, degree]
+                file.write(",".join(str(x) for x in l))
+                file.write(",")
+                file.write(",".join(str(x) for x in errors))
+                file.write("\n")
+                
