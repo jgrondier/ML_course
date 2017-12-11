982db1edff50e6acaf2b92650d1259987f87ce92
gan74 <gregoire.angerand@epfl.ch>
2017-12-11 16:16:39 +0100

    add gregs.py

---
+++ projects/project2/project_road_segmentation/gregs.py
@@ -0,0 +1,181 @@
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+import os,sys
+from PIL import Image
+from sklearn import linear_model
+
+
+# Helper functions
+
+def load_image(infilename):
+    data = mpimg.imread(infilename)
+    return data
+
+def img_float_to_uint8(img):
+    rimg = img - np.min(img)
+    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
+    return rimg
+
+# Concatenate an image and its groundtruth
+def concatenate_images(img, gt_img):
+    nChannels = len(gt_img.shape)
+    w = gt_img.shape[0]
+    h = gt_img.shape[1]
+    if nChannels == 3:
+        cimg = np.concatenate((img, gt_img), axis=1)
+    else:
+        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
+        gt_img8 = img_float_to_uint8(gt_img)          
+        gt_img_3c[:,:,0] = gt_img8
+        gt_img_3c[:,:,1] = gt_img8
+        gt_img_3c[:,:,2] = gt_img8
+        img8 = img_float_to_uint8(img)
+        cimg = np.concatenate((img8, gt_img_3c), axis=1)
+    return cimg
+
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
+
+
+# Extract 6-dimensional features consisting of average RGB color as well as variance
+def extract_features(img):
+    feat_m = np.mean(img, axis=(0,1))
+    feat_v = np.var(img, axis=(0,1))
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+# Extract 2-dimensional features consisting of average gray color as well as variance
+def extract_features_2d(img):
+    feat_m = np.mean(img)
+    feat_v = np.var(img)
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+# Extract features for a given image
+def extract_img_features(filename):
+    img = load_image(filename)
+    img_patches = img_crop(img, patch_size, patch_size)
+    X = np.asarray([ extract_features_2d(img_patches[i]) for i in range(len(img_patches))])
+    return X
+
+# Convert array of labels to an image
+def label_to_img(imgwidth, imgheight, w, h, labels):
+    im = np.zeros([imgwidth, imgheight])
+    idx = 0
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            im[j:j+w, i:i+h] = labels[idx]
+            idx = idx + 1
+    return im
+
+def make_img_overlay(img, predicted_img):
+    w = img.shape[0]
+    h = img.shape[1]
+    color_mask = np.zeros((w, h, 3), dtype=np.uint8)
+    color_mask[:,:,0] = predicted_img*255
+
+    img8 = img_float_to_uint8(img)
+    background = Image.fromarray(img8, 'RGB').convert("RGBA")
+    overlay = Image.fromarray(color_mask, 'RGB').convert("RGBA")
+    new_img = Image.blend(background, overlay, 0.2)
+    return new_img
+
+
+
+
+
+
+
+
+
+
+
+# Loaded a set of images
+root_dir = "training/"
+
+image_dir = root_dir + "images/"
+files = os.listdir(image_dir)
+n = min(20, len(files)) # Load maximum 20 images
+print("Loading " + str(n) + " images")
+imgs = [load_image(image_dir + files[i]) for i in range(n)]
+print(files[0])
+
+gt_dir = root_dir + "groundtruth/"
+print("Loading " + str(n) + " images")
+gt_imgs = [load_image(gt_dir + files[i]) for i in range(n)]
+print(files[0])
+
+n = 10 # Only use 10 images for training
+
+# Extract patches from input images
+patch_size = 16 # each patch is 16*16 pixels
+
+img_patches = [img_crop(imgs[i], patch_size, patch_size) for i in range(n)]
+gt_patches = [img_crop(gt_imgs[i], patch_size, patch_size) for i in range(n)]
+
+# Linearize list of patches
+img_patches = np.asarray([img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))])
+gt_patches =  np.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
+
+# Compute features for each image patch
+foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
+
+def value_to_class(v):
+    df = np.sum(v)
+    if df > foreground_threshold:
+        return 1
+    else:
+        return 0
+
+X = np.asarray([ extract_features_2d(img_patches[i]) for i in range(len(img_patches))])
+Y = np.asarray([value_to_class(np.mean(gt_patches[i])) for i in range(len(gt_patches))])
+
+print('Computed ' + str(X.shape[0]) + ' features')
+print('Feature dimension = ' + str(X.shape[1]))
+print('Number of classes = ' + str(np.max(Y)))
+
+Y0 = [i for i, j in enumerate(Y) if j == 0]
+Y1 = [i for i, j in enumerate(Y) if j == 1]
+print('Class 0: ' + str(len(Y0)) + ' samples')
+print('Class 1: ' + str(len(Y1)) + ' samples')
+
+
+# we create an instance of the classifier and fit the data
+logreg = linear_model.LogisticRegression(C=1e5, class_weight="balanced")
+logreg.fit(X, Y)
+
+# Predict on the training set
+Z = logreg.predict(X)
+
+# Get non-zeros in prediction and grountruth arrays
+Zn = np.nonzero(Z)[0]
+Yn = np.nonzero(Y)[0]
+
+TPR = len(list(set(Yn) & set(Zn))) / float(len(Z))
+print('True positive rate = ' + str(TPR))
+
+# Run prediction on the img_idx-th image
+img_idx = 12
+
+Xi = extract_img_features(image_dir + files[img_idx])
+Zi = logreg.predict(Xi)
+
+w = gt_imgs[img_idx].shape[0]
+h = gt_imgs[img_idx].shape[1]
+predicted_im = label_to_img(w, h, patch_size, patch_size, Zi)
+cimg = concatenate_images(imgs[img_idx], predicted_im)
+
+new_img = make_img_overlay(imgs[img_idx], predicted_im)
+new_img.show()
