b65690aa7ce753aac3ffc32eb3049926f21e0258
gan74 <gregoire.angerand@epfl.ch>
2017-12-14 19:38:18 +0100

    idk

--- projects/project2/project_road_segmentation/greg.py
+++ projects/project2/project_road_segmentation/greg.py
@@ -17,8 +17,9 @@ except:
 
 
 
+# -------------------------------- segmentation -------------------------------
 
-def compute_segmentation(image, sig = 0.5, footprint = 10):
+def compute_segmentation(image, sig = 1.0, footprint = 10):
     grayscale = helpers.to_lum(image)
     cn = np.where(feature.canny(grayscale, sigma = sig), False, True)
 
@@ -34,13 +35,11 @@ def compute_segmentation(image, sig = 0.5, footprint = 10):
     return labels, markers, distance, cn
 
 
-def group_data(image, labels, group):
-    return image[np.where(labels == group)]
+# -------------------------------- groups -------------------------------
 
-def group_features(data):
-    if len(data) == 0:
-        return [0, 0.0, 0.0]
-    return [len(data), np.mean(data), np.var(data)]
+def group_data(image, labels, group):
+    w = np.where(labels == group)
+    return image[w]
 
 def group_truth(gt_image, labels, group, threshold = 0.25):
     df = np.sum(gt_image[np.where(labels == group)])
@@ -76,8 +75,37 @@ def predicted_image(labels, Zi):
 
 
 
+# -------------------------------- features -------------------------------
+
+def group_features(data):
+    if len(data) == 0:
+        print("EMPTY GROUP")
+
+    sat = np.max(data)
+    r = data[:, 0] / sat
+    g = data[:, 1] / sat 
+    b = data[:, 2] / sat
+    lum = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    lum_m = np.mean(lum)
+    mini = np.min(np.c_[r, g, b])
+    maxi = np.max(np.c_[r, g, b])
+    return [lum_m, mini, maxi, np.var(lum)]
 
 
+
+
+
+
+# -------------------------------- debug -------------------------------
+
+def shuffle_labels(labels):
+    uniqs = np.unique(labels)
+    sh = np.copy(uniqs)
+    np.random.shuffle(sh)
+    shuffled = np.zeros(labels.shape)
+    for s, l in zip(sh, uniqs):
+        shuffled[labels == l] = s
+    return shuffled
 
 
 def quick_visu():
@@ -115,6 +143,9 @@ def quick_visu():
     plt.show()
 
 
+
+# -------------------------------- learning -------------------------------
+
 def learn(imgs, gt_imgs):
     groups = []
     truths = []
@@ -130,7 +161,7 @@ def learn(imgs, gt_imgs):
             truths.append(group_truth(gt_image, labels, group))
 
     X = np.asarray(groups)
-    Y = np.where(np.asarray(truths), -1.0, 1.0)
+    Y = np.where(np.asarray(truths), -1.0, 1.0) # this is so that we don't work on bools cause they make everything more complicated
 
     logreg = linear_model.LogisticRegression(C = 1e5, class_weight = "balanced")
     logreg.fit(X, Y)
@@ -141,36 +172,51 @@ def predict(image, logreg):
     Xi, labels = image_features(image)
     Zi = logreg.predict(Xi)
     print("scores in ", np.unique(Zi))
-    return predicted_image(labels, Zi), labels
+    return predicted_image(labels, Zi)
 
 
 
 
 
 
+# -------------------------------- main -------------------------------
 
-imgs, gt_imgs = helpers.load_training_set("training/", 99999)
-max_train = 17
+max_train = 99
+refs, imgs, gt_imgs = helpers.load_training_set("training/", max_train + 1)
 
 logreg = learn(imgs[:max_train], gt_imgs[:max_train])
 
+ref = refs[max_train]
 image = imgs[max_train]
 gt_image = gt_imgs[max_train]
-pred, labels = predict(image, logreg)
+pred = predict(image, logreg)
 
 
-fig, axes = plt.subplots(ncols = 3, nrows = 2, subplot_kw = {'adjustable': 'box-forced'})
+labels, _, _, canny = compute_segmentation(image)
+
+fig, axes = plt.subplots(ncols = 2, nrows = 2, subplot_kw = {'adjustable': 'box-forced'})
 ax = axes.ravel()
 
-ax[0].imshow(image, interpolation = 'nearest')
+ax[0].imshow(ref, interpolation = 'nearest')
 ax[0].set_title('Reference')
 ax[1].imshow(pred, cmap = plt.cm.spectral, interpolation = 'nearest')
 ax[1].set_title('Prediction')
-ax[2].imshow(labels, cmap = plt.cm.spectral, interpolation = 'nearest')
-ax[2].set_title('Separated objects')
-ax[3].imshow(gt_image, cmap = plt.cm.gray, interpolation = 'nearest')
-ax[3].set_title('Ground truth')
-ax[4].imshow(group_truth_image(image, gt_image), cmap = plt.cm.gray, interpolation = 'nearest')
-ax[4].set_title('Per group truth')
+ax[2].imshow(gt_image, cmap = plt.cm.gray, interpolation = 'nearest')
+ax[2].set_title('Ground truth')
+ax[3].imshow(group_truth_image(image, gt_image), cmap = plt.cm.gray, interpolation = 'nearest')
+ax[3].set_title('Per group truth')
+fig.tight_layout()
+
+
+plt.figure(1)
+fig, axes = plt.subplots(ncols = 3, nrows = 1, subplot_kw = {'adjustable': 'box-forced'})
+ax = axes.ravel()
+
+ax[0].imshow(shuffle_labels(labels), cmap = plt.cm.spectral, interpolation = 'nearest')
+ax[0].set_title('Separated objects')
+ax[1].imshow(image, interpolation = 'nearest')
+ax[1].set_title('Processed reference')
+ax[2].imshow(canny, cmap = plt.cm.gray, interpolation = 'nearest')
+ax[2].set_title('Canny')
 fig.tight_layout()
 plt.show()
--- projects/project2/project_road_segmentation/helpers.py
+++ projects/project2/project_road_segmentation/helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.image as mpimg
+from scipy import ndimage as ndi
 import os, sys
 
 def load_image(infilename):
@@ -16,8 +17,38 @@ def load_training_set(root_dir, max_images = 20):
     
     imgs    = [load_image(im_dir + files[i]) for i in range(n)]
     gt_imgs = [load_image(gt_dir + files[i]) for i in range(n)]
+    pr_imgs    = [process(i) for i in imgs]
 
-    return imgs, gt_imgs
+    return imgs, pr_imgs, gt_imgs
+
+def process(rgb):
+    return rgb
+
+def sharpen(rgb, alpha = 0.1):
+    blurred = blur(rgb, 1.0)
+    return np.clip(rgb + alpha * (rgb - blurred), 0.0, 255.0)
+
+
+def median(rgb, size = 5):
+    bl = np.zeros(rgb.shape)          
+    bl[:, :, 0] = ndi.median_filter(rgb[:, :, 0], size)
+    bl[:, :, 1] = ndi.median_filter(rgb[:, :, 1], size)
+    bl[:, :, 2] = ndi.median_filter(rgb[:, :, 2], size)
+    return bl
+
+def blur(rgb, sig = 3.0):
+    bl = np.zeros(rgb.shape)          
+    bl[:, :, 0] = ndi.gaussian_filter(rgb[:, :, 0], sig)
+    bl[:, :, 1] = ndi.gaussian_filter(rgb[:, :, 1], sig)
+    bl[:, :, 2] = ndi.gaussian_filter(rgb[:, :, 2], sig)
+    return bl
+
+def saturate(rgb, amount):
+    bl = np.zeros(rgb.shape)          
+    bl[:, :, 0] = ndi.gaussian_filter(rgb[:, :, 0], sig)
+    bl[:, :, 1] = ndi.gaussian_filter(rgb[:, :, 1], sig)
+    bl[:, :, 2] = ndi.gaussian_filter(rgb[:, :, 2], sig)
+    return bl
 
 def to_bool(gray, threshold = 0.75):
     return gray > threshold
@@ -28,7 +59,7 @@ def to_lum(rgb):
     b = rgb[:, :, 2]
     return 0.2989 * r + 0.5870 * g + 0.1140 * b
 
-def to_gray_(rgb):
+def to_gray(rgb):
     r = rgb[:, :, 0]
     g = rgb[:, :, 1]
     b = rgb[:, :, 2]
